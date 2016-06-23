b3a0d295de135c1c48cc53f440f062d62b0aa5d3
Fabiano <fdolzanes1@gmail.com>
2016-06-23 20:00:27 +0000

    update index

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $('#navbar-collapse').on('show.bs.collapse', function() {
-    $('.chamada-principal').css('transform', 'translate(-50%, 10%)');
+    $('.topCasaFina-banner').css('transform', 'translate(-50%, 10%)');
 });
 
 $('#navbar-collapse').on('hide.bs.collapse', function() {
-    $('.chamada-principal').css('transform', 'translate(-50%, 50%)');
+    $('.topCasaFina-banner').css('transform', 'translate(-50%, -50%)');
 });
